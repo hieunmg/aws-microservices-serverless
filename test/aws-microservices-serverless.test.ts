64a219f060c0a77f608ae9aac02bee928e85344f
Hieu Minh <hieu.nguyen@pionero.io>
2023-05-10 21:06:44 +0700

    init

---
+++ test/aws-microservices-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsMicroservicesServerless from '../lib/aws-microservices-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-microservices-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsMicroservicesServerless.AwsMicroservicesServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
